19d7240b031804b0fc7896db8f9378814fcb50ef
gurleen-matharoo <42582011+gurleen-matharoo@users.noreply.github.com>
2019-06-05 18:54:48 -0400

    Delete debug.h

--- debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUG_H
-#define	DEBUG_H
-
-void dbgOutputVal(unsigned int outVal);
-void dbgUARTVal(unsigned char outVal);
-void dbgOutputLoc(unsigned int outVal);
-void dbgError();
-
-#endif	/* DEBUG_H */
